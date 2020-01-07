7658c2b0939ab425512cd5018e589307e378ab61
Khanukay <vestost5@gmail.com>
2020-01-07 21:52:00 +0200

    changed file after adding cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import run
+
 def greet():
-    print ('Welcome to Brain Games!')
+    print ('Welcome to Brain Games!\n')
+    
+    print(run())
+
 
 def main():
 	greet()
