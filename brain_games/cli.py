39a395a5c4994f0c398cc6b4ef53b04467f3c5f1
Khanukay <vestost5@gmail.com>
2020-01-07 21:49:18 +0200

    add

---
+++ brain_games/cli.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import prompt
+
+def run():
+	name = prompt.string('May I have your name? ')
+	return ('Hello, ' + name + '!')
+
+#print(run())
+
+def main():
+	run()
+
+if __name__ == '__main__':
+    main()
